25ceae92bb4a9cbd2e2e768e05b0d0a3fa5c9451
lemaedu <lemaedu@gmail.com>
2021-10-19 22:18:15 -0500

    100. MovieDetails - Slivers - SliverAppBar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'NO MUVIE';
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Details Screen'),
+        body: CustomScrollView(
+      //Sliver son widgets preprogramado cuando se hace scroll en el padre
+      slivers: [_CustomAppBar()],
+    ));
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true, //para que nunca desaparezca
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: Text(
+            'Titulo de la pelicula',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          width: 130,
+          height: 200,
+          fit: BoxFit.cover,
         ),
       ),
     );
